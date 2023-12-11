75564cdb06bd51c5232e19f53e03c79796909cff
Gustavo Souto <guSDev.Pro@gmail.com>
2023-12-11 17:02:54 -0300

    Criando Model Category

--- contact/admin.py
+++ contact/admin.py
@@ -9,4 +9,9 @@ class ContactAdmin(admin.ModelAdmin):
     search_fields = 'id', 'first_name', 'last_name',
     list_per_page = 10
     list_max_show_all = 200
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',
     
